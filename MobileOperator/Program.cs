9848b21fd155604b9a5501f6a87f359d066a1bba
KolyaBLR <bk97w@bk.ru>
2017-05-22 01:06:01 +0400

    I Kolya

--- MobileOperator/Program.cs
+++ MobileOperator/Program.cs
@@ -17,6 +17,7 @@ namespace MobileOperator
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new OwnerForm());
+            //Тут был Коля
         }
     }
 }
