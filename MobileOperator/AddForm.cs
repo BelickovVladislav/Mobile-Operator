3bb61300b4925bcd1c22dabddad7d1a0392c88fa
KolyaBLR <bk97w@bk.ru>
2017-05-23 14:27:39 +0400

    addForm fix

--- MobileOperator/AddForm.cs
+++ MobileOperator/AddForm.cs
@@ -13,6 +13,35 @@ namespace MobileOperator
 {
     public partial class AddForm : Form
     {
-        
+        private object Property;
+
+        public AddForm(string property)
+        {
+            InitializeComponent();
+            Property = property;
+            switch (Property)
+            {
+                case "Producer": Text = "Добавления производителя"; break;
+                case "PhoneType": Text = "Добавления типа телефона"; break;
+                case "OS": Text = "Добавления операционной системы"; break;
+            }
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            if (AddBox.Text != "")
+            {
+                switch (Property)
+                {
+                    case "Producer": Producer.Add(AddBox.Text); break;
+                    case "PhoneType": PhoneType.Add(AddBox.Text); break;
+                    case "OS": OS.Add(AddBox.Text); break;
+                }
+                MessageBox.Show("Категория успешно добавлена.");
+                Close();
+            }
+            else
+                MessageBox.Show("Заполните поле категории.");
+        }
     }
 }
